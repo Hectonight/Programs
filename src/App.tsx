4b6a13aec1c41104cca7e1d8dd0a9d0b6346011b
Hectonite-code <72630507+Hectonite-code@users.noreply.github.com>
2022-09-12 08:14:43 -0400

    Fix Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import data from "./data";
 import Program from "./Programs";
 
 function App(): JSX.Element {
-  const elements: JSX.Element[] = data.map((dat, index) =>
-    Program({ ...dat, key: index })
-  );
+  const elements: JSX.Element[] = data.map((dat, index) => (
+    <Program
+      key={index}
+      name={dat.name}
+      website={dat.website}
+      shadow_color={dat.shadow_color}
+    ></Program>
+  ));
 
-  return <div className="App">{elements}</div>;
+  return (
+    <div className="App">
+      <h1>Programs</h1>
+      <div className="Elements">{elements}</div>
+    </div>
+  );
 }
 
 export default App;
